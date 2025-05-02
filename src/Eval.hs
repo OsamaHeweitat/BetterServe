0c856630de02294ad44e8b8bd8c99eabac4d8a1b
OsamaHeweitat <osamaheweitatdev@gmail.com>
2025-05-02 16:10:01 +0100

    Commit

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,7 +1,26 @@
-module Eval where 
+module Eval where
 import Grammar
 
-type Env = [(String, Int)]
+type Table = (String, [[String]]) -- (TableName, Rows)
+type Column = (Int, [String]) -- (ColumnIndex, ColumnValues)
 
-eval :: Env -> Exp -> Int
-eval = undefined
+evalStmt :: Statement -> IO [String]
+evalStmt (SelectStmt selection tabs optionals) = do
+    tables <- evalTables tabs
+    result <- evalSelection selection tables
+    result <- evalOptionals optionals result
+
+evalTables :: [String] -> IO [Table]
+
+evalSelection :: Selection -> [Table] -> [Column]
+evalSelection SelectAll tables = tables
+evalSelection (SelectColumns columns) tables = map (evalColumns columns) tables
+
+readCSV :: String -> IO Table
+readCSV filename = do
+    content <- readFile filename
+    let filenameWithoutExt = takeWhile (/= '.') filename
+    let rows = lines content
+    let header = words (head rows)
+    let dataRows = map words (tail rows)
+    return (filenameWithoutExt, header : dataRows)
