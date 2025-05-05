fa2d73e294563c9323d73d872eef1b9c1285f5f5
OsamaHeweitat <osamaheweitatdev@gmail.com>
2025-05-05 12:40:38 +0100

    Everything is awesome

--- src/Eval.hs
+++ src/Eval.hs
@@ -4,24 +4,37 @@ import Grammar
 type Table = (String, [[String]]) -- (TableName, Rows)
 type ColumnType = (Int, [String]) -- (ColumnIndex, ColumnValues)
 
+data Value
+    = TableVal [[String]]
+    | StrVal String
+    | IntVal Int
+    | BoolVal Bool
+    deriving (Show, Eq)
+
 evalStmt :: Statement -> IO [String]
 evalStmt (SelectStmt selection tabs optionals) = do
     tables <- evalTables tabs
     result <- evalSelection selection tables
     result <- evalOptionals optionals result
 
-evalTables :: [String] -> [Table]
-evalTables [] = return []
-evalTables (t:ts) = do
-    table <- readCSV t
-    rest <- evalTables ts
-    return (table : rest)
+evalTables :: Tables -> [Table]
+evalTables (LoadTable filename) = do
+    contents <- readFile filename
+    let rows = map (splitOn ",") (lines contents)
+    return [(filename, rows)]
 
 evalSelection :: Selection -> [Table] -> [Column]
 evalSelection SelectAll tables = tables
 evalSelection (SelectColumns columns) tables = map (evalColumns columns) tables
 
-evalColumns :: [Column] -> [Table] -> Column
+evalColumns :: [Column] -> [Table] -> [ColumnType]
+evalColumns columns tables = do
+    let columnIndex = map (getColumnIndex columns) tables
+    let columnValues = map (getColumnValues columnIndex) tables
+    return (columnIndex, columnValues)
+
+evalOptionals :: [Optional] -> [Column] -> [Table] -> [String]
+evalOptionals 
 
 readCSV :: String -> IO Table
 readCSV filename = do
